baaf881ffa814f1a892fc688ac8a20ed6bccf556
julian <jurianu1214@gmail.com>
2022-06-16 10:27:21 +0200

    todo

---
+++ app.js
@@ -0,0 +1,26 @@
+/* require('./database/mongo.cnx') */
+
+const express = require("express");
+const app = express();
+require("./database/mongo")
+require("./database/sqlDataBase")
+
+const router = require("./routes/routes")
+const PORT = process.env.PORT || 3001;
+ 
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static(__dirname + "/views/css"));
+
+ 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+ 
+app.use("/", router);
+
+app.listen(PORT, ()=>{
+    console.log("Se inicia el server en : http://localhost:${PORT}");
+})
+
