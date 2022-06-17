5019be573113d7d43a03376f005ab3e493771a74
Alberto Lara <albertolaracaro@gmail.com>
2022-06-17 20:35:02 +0200

    Pintamos uCuber

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -7,19 +7,36 @@ const mysql = require("mysql")
 
 const user = {
   saveDataForm: (req, res) => {
-    nombre = req.body.name;
-    apellidos = req.body.surname;
-    email = req.body.email;
-    contrasena = req.body.pass;
-    contrasenaConf = req.body.passConf;
-    telefono = req.body.phone;
-    dni = req.body.dni;
+    let nombre = req.body.name;
+    let apellidos = req.body.surname;
+    let email = req.body.email;
+    let contrasena = req.body.pass;
+    let contrasenaConf = req.body.passConf;
+    let telefono = req.body.phone;
+    let dni = req.body.dni;
     const emailExp = new RegExp(/^([\d\w_\.-]+)@([\d\w\.-]+)\.([\w\.]{3})$/);
     const nameExp = new RegExp(/^([A-Za-z]{1,15})$/);
     const unNameExp = new RegExp(/^([A-Za-z]{1,15})$/);
     const telfExp = new RegExp(/^\d{9}$/);
     const dniExp = new RegExp(/^\d{8}[a-zA-Z]$/);
     const passExp = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/);
+
+
+    /*let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?';
+    //`SELECT * FROM Usuarios WHERE email = ${loginEmail}`
+    let query3 = mysql.format(selectQuery, ["Usuarios", "email", email, "dni", dni]);
+    console.log("selectQuery" + selectQuery)
+    console.log("query3" + query3)
+    connection.query (query3, async (err, data) => {
+      if (err) throw err; 
+      const emailRepetido = await data[0].email;
+      const dniRepetido = await data[0].dni;
+      if (email == emailRepetido || dni == dniRepetido) {
+        console.log("Usuario ya registrado");
+        await res.render("index", { usuarioRegistrado: "Usuario ya registrado" });
+      }*/
+
+
     if (
       !emailExp.test(email) ||
       !nameExp.test(nombre) ||
@@ -28,9 +45,11 @@ const user = {
       !passExp.test(contrasena) ||
       contrasena != contrasenaConf ||
       !telfExp.test(telefono)
-    ) {
+    ) { 
       console.log("campos incorrectos"); //renderizar una pagina de campos incorrectos
     } else {
+      
+
       let insertQuery = `INSERT INTO Usuarios
       (
           nombre, apellido, dni ,email, telefono, contrasena
@@ -47,16 +66,9 @@ const user = {
         if (err) throw err;
         console.log(data);
       });
-      console.log("todo ok");
+      res.render("index" , { usuarioRegistrado: "Usuario registrado correctamente" });
     }
 
-
-
-
-    //regex
-
-
-    //condicion de comprobacion
 
   },
   registerUser: (req, res) => {
@@ -66,59 +78,99 @@ const user = {
 
   },
   login: (req, res) => {
-    loginEmail = req.body.email
+    loginEmail = req.body.userLog
     passLog = req.body.passLog
-    
-    let nameCorrect = `SELECT * FROM Usuarios`;
+
+
+    let nameCorrect = `SELECT email,contrasena FROM Usuarios`;
 
     connection.query(nameCorrect, (err, rows) => {
-      if(err) throw err;
+      if (err) throw err;
       console.log('Usuario: \n', rows);
+
       //res.json(rows);
-      res.render("uCuber")
-      
 
-      //connection.end();
-    }
-    )
+      for (let i = 0; i < rows.length; i++) {
+
+        if (rows[i].email == loginEmail && rows[i].contrasena == passLog) {
+          //res.render("uCuber") 
+          let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?';
+    // //`SELECT * FROM Usuarios WHERE email = ${loginEmail}`
+    let query3 = mysql.format(selectQuery, ["Usuarios", "email", loginEmail]);
+    console.log("selectQuery" + selectQuery)
+    console.log("query3" + query3)
+    connection.query(query3, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+      let logNombre = data[0].nombre;
+      let logApellido = data[0].apellido;
+      let logDni = data[0].dni;
+      let logEmail = data[0].email;
+      let logTelefono = data[0].telefono;
+      res.render("uCuber", {logNombre, logApellido, logDni, logEmail, logTelefono});
+    }) } else {
+        
+        //res.render("index", {logError: "Usuario o contraseña incorrectos"}) 
+          
+          
+        }
+        
+      }
+
+    });
+
+    // let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?';
+    // //`SELECT * FROM Usuarios WHERE email = ${loginEmail}`
+    // let query3 = mysql.format(selectQuery, ["Usuarios", "email", loginEmail]);
+    // console.log("selectQuery" + selectQuery)
+    // console.log("query3" + query3)
+    // connection.query(query3, (err, data) => {
+    //   if (err) throw err;
+    //   console.log(data);
+    //   let logNombre = data[0].nombre;
+    //   let logApellido = data[0].apellido;
+    //   let logDni = data[0].dni;
+    //   let logEmail = data[0].email;
+    //   let logTelefono = data[0].telefono;
+    //   res.render("uCuber", {logNombre});
+    // })
+
+
+
+
+
+
+
+    //res.render("uCuber")
+
+
+    //connection.end();
+
+
   },
-  logHome: (req,res) => {
-    res.render("indexLog")
-  }
-  
+  logHome: (req, res) => {
+    console.log("hola");
+    // loginEmail = req.body.userLog
+    // let selectQuery = 'SELECT * FROM ?? WHERE ?? = ?';
+    // let query3 = mysql.format(selectQuery, ["Usuarios", "email", loginEmail]);
+    // console.log("selectQuery" + selectQuery)
+    // console.log("query3" + query3)
+    // connection.query(selectQuery, (err, data) => {
+    //   if (err) throw err;
+    //   console.log(data);
+    //   //connection.end();
+    // });
 
-  //'SELECT * FROM ?? WHERE ?? = ?';    
+  }
+
+
+
 
 }
-//   const login = {
-//    login: (req, res) => {
-//       loginEmail = req.body.email
-//       passLog = req.body.passLog
-
-//       console.log(loginEmail);
-//       console.log(passLog);
-//   }
-//     let nameCorrect = `SELECT * FROM Usuarios`;
-
-//     connection.query(nameCorrect, (err, rows) => {
-//       if(err) throw err;
-//       console.log('Usuario: \n', rows);
-
-//       //connection.end();
-//     }
-//     )
-
-//   }
-
-//   }
-// }
 
 
 
-//connection.end();
 
-
-// module.exports = user.login;
 module.exports = user;
-// module.exports = login;
+
 
--- database/sqlDataBase.js
+++ database/sqlDataBase.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root1234",
+  password: "root",
   database: "cuberRelacional",
 });
 
